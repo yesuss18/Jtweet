fca6f79bdca85bd7d9d6b12f6947c4302babd765
yessuss18 <yessuss18@gmail.com>
2014-08-17 16:25:35 -0500

    usando plantilla

--- jtweet/jtweet/jtweetapp/views.py
+++ jtweet/jtweet/jtweetapp/views.py
@@ -1,7 +1,9 @@
-from django.shortcuts import render
-from django.http import HttpResponse
+from django.shortcuts import render_to_response
+from django.template import RequestContext
 
 # Create your views here.
 
 def index(request):
-	return HttpResponse('Hola segunda clase')
+	return render_to_response('jtweetapp/index.html',
+		                       {},
+		                       context_instance=RequestContext(request))
